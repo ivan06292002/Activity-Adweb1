1f175d55cd4dfad3c6580d283a65bc9373908813
ivan06292002 <100040157+ivan06292002@users.noreply.github.com>
2023-01-24 13:04:28 +0800

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component, VERSION } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  //name = 'Angular ' + VERSION.major;
+  appName = 'This is a One-way Data binding example.';
+  clientName: string = 'Ivan';
+  appliedCSSClass = 'green';
+  notappliedCSSClass = false;
+  myColor = 'red';
+  showData($event: any) {
+    console.log('button is clicked!');
+    if ($event) {
+      console.log($event.target);
+      console.log($event.target.value);
+    }
+  }
+}
